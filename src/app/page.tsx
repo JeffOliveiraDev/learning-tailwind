4b3df1c78c80a931df23c230c2fb344221dbd8ef
jeffOliveiraDev <jffrso@gmail.com>
2023-08-31 17:41:22 -0300

    feat: interface completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,57 +3,27 @@ import {
   ChevronRight,
   Home as HomeIcon,
   Library,
+  List,
+  Maximize2,
+  Mic2,
+  MonitorSmartphone,
+  Play,
+  Repeat,
   Search,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Volume1,
 } from "lucide-react";
 import Image from "next/image";
+import Sidebar from "./../components/Sidebar";
+import Footer from "./../components/Footer";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1 ">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded- full" />
-            <div className="w-3 h-3 bg-yellow-300 rounded- full" />
-            <div className="w-3 h-3 bg-green-500 rounded- full" />
-          </div>
-          <nav className="space-y-5 mt-10 rounded">
-            <a
-              href=""
-              className="flex items-center gap-3 text-xs font-semibold text-zinc-200"
-            >
-              <HomeIcon />
-              Home
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-xs font-semibold text-zinc-200"
-            >
-              <Search />
-              Buscar
-            </a>
-
-            <a
-              href=""
-              className="flex items-center gap-3 text-xs font-semibold text-zinc-200"
-            >
-              <Library />
-              Biblioteca
-            </a>
-          </nav>
-
-          <nav className="mt-6 pt-10 rounded  border-t border-zinc-800 flex flex-col gap-1">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Elevate
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Treino Pesado
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Pop Nacional
-            </a>
-          </nav>
-        </aside>
+        <Sidebar />
         <main className="flex-1 p-6">
           <div className="flex items-center gap-4">
             <button className="rounded- full bg-black/40 p-1">
@@ -66,10 +36,10 @@ export default function Home() {
 
           <h1 className="font-semibold text-3xl mt-8">Bem vindo!</h1>
 
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="grid grid-cols-3 gap-4 mt-4 ">
             <a
               href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
             >
               <Image
                 src="/Night.webp"
@@ -78,10 +48,29 @@ export default function Home() {
                 alt="Capa do Álbum"
               />
               <strong>Melhor Álbum</strong>
+              <button className="shadow-inner shadow-black invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 ">
+                <Play />
+              </button>
+            </a>
+
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+              />
+              <strong>Melhor Álbum</strong>
+              <button className="invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
             </a>
             <a
               href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
             >
               <Image
                 src="/Night.webp"
@@ -90,10 +79,13 @@ export default function Home() {
                 alt="Capa do Álbum"
               />
               <strong>Melhor Álbum</strong>
+              <button className="invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
             </a>
             <a
               href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors "
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
             >
               <Image
                 src="/Night.webp"
@@ -102,10 +94,13 @@ export default function Home() {
                 alt="Capa do Álbum"
               />
               <strong>Melhor Álbum</strong>
+              <button className="invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
             </a>
             <a
               href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors "
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
             >
               <Image
                 src="/Night.webp"
@@ -114,10 +109,13 @@ export default function Home() {
                 alt="Capa do Álbum"
               />
               <strong>Melhor Álbum</strong>
+              <button className="invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
             </a>
             <a
               href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors "
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors"
             >
               <Image
                 src="/Night.webp"
@@ -126,25 +124,104 @@ export default function Home() {
                 alt="Capa do Álbum"
               />
               <strong>Melhor Álbum</strong>
+              <button className="invisible flex group-hover:visible w-10 h-10  items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-8">Feito para Você!</h2>
+
+          <div className="grid grid-cols-6 gap-4 mt-4">
+            <a
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
             </a>
             <a
-              href="#"
-              className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors "
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
             >
               <Image
                 src="/Night.webp"
                 width={104}
                 height={104}
                 alt="Capa do Álbum"
+                className="w-full"
               />
-              <strong>Melhor Álbum</strong>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
+            </a>
+            <a
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
+            </a>
+            <a
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
+            </a>
+            <a
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
+            </a>
+            <a
+              href=""
+              className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10"
+            >
+              <Image
+                src="/Night.webp"
+                width={104}
+                height={104}
+                alt="Capa do Álbum"
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">NightWish Forever</span>
             </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
-      </footer>
+
+      <Footer />
     </div>
   );
 }
